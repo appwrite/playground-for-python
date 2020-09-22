82aa0fc96bcc868c9925cd60a908e45c819a86a1
Nibba2018 <sohambiswas41@gmail.com>
2020-09-22 14:28:53 +0530

    Make api calls synchronous

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from appwrite.services.users import Users
 from appwrite.services.database import Database
 from appwrite.services.storage import Storage
 
-import asyncio
+import datetime
 
 # Helper method to print green colored output.
 def print_green(prt):
@@ -12,8 +12,8 @@ def print_green(prt):
 # Config
 
 ENDPOINT = 'https://localhost/v1'
-PROJECT_ID = '5f67b5ada134c'
-API_KEY = '899b139bdca90746a849f8b31567e378d6e41a0e6667ce5826ce2bbee9caba844b0f5927a61e838d104dfee16c6d411a29b066b7f258ce28f79996f752f8c14ccacb44b7d3f801a8d4047320a8b1011c0332fcc3aedfce4337b8468563a02665ab945b83cefa94cf9922553a1fe7c411314db0c7c9dee2fc36af7e086068b69f'
+PROJECT_ID = '5f69ba7d23216'
+API_KEY = 'e8648fb26ea100a1ddca908adb2802a73081243621f7a228435e4b84b8aa1f25a9355af571e0c9c29c8b4840e129aa4160ea43194bd6620fc4520828becd243be25b332cd91c68d1a21a5171a079828e006321060b416189ec7814d0df2d4eb961f39b2ba5de51e6bd69ab8ccf1722f97e9aca86596b0d65247720917411b83e'
 
 client = Client()
 
@@ -35,10 +35,11 @@ userId = None
 
 # List of API definations
 
-async def create_collection():
+def create_collection():
+    global collectionId
     database = Database(client)
     print_green("Running Create Collection API")
-    response = await database.create_collection(
+    response = database.create_collection(
         'Movies',
         ['*'],
         ['*'],
@@ -49,22 +50,22 @@ async def create_collection():
              'default': 1970, 'required': True, 'array': False}
         ]
     )
-    collectionId = response.id
+    collectionId = response['$id']
     print(response)
 
 
-async def list_collection():
+def list_collection():
     database = Database(client)
     print_green("Running List Collection API")
-    response = await database.list_collections()
-    collection = response.collections[0]
+    response = database.list_collections()
+    collection = response['collections'][0]
     print(collection)
 
 
-async def add_doc():
+def add_doc():
     database = Database(client)
     print_green("Running Add Document API")
-    response = await database.create_document(
+    response = database.create_document(
         collectionId,
         {
             'name': "Spider Man",
@@ -76,57 +77,61 @@ async def add_doc():
     print(response)
 
 
-async def list_doc():
+def list_doc():
     database = Database(client)
     print_green("Running List Document API")
-    response = await database.list_documents(collectionId)
+    response = database.list_documents(collectionId)
     print(response)
 
 
-async def upload_file():
+def upload_file():
     storage = Storage(client)
     print_green("Running Upload File API")
-    response = await storage.create_file(
+    response = storage.create_file(
         open("./nature.jpg", 'rb'),
         [],
         []
     )
 
 
-async def create_user(email, password, name):
+def create_user(email, password, name):
+    global userId
     users = Users(client)
     print_green("Running Create User API")
-    response = await users.create(
+    response = users.create(
         email,
         password,
         name
     )
-    userId = response.id
+    userId = response['$id']
     print(response)
 
 
-async def list_user():
+def list_user():
     users = Users(client)
     print_green("Running List User API")
-    response = await users.list()
+    response = users.list()
     print(response)
 
 
-async def run_all_tasks():
+def run_all_tasks():
 
-    await asyncio.wait([
-        await create_collection(),
-        await list_collection(),
-        await add_doc(),
-        await list_doc(),
-        await upload_file(),
-        await create_user(),
-        await list_user(),
-    ])
+    name = str(datetime.datetime.now()).split()[0]
+
+    create_collection()
+    list_collection()
+    add_doc()
+    list_doc()
+    upload_file()
+    create_user(
+        name + '@test.com',
+        name + '@123',
+        name
+    )
+    list_user()
 
 
 if __name__ == "__main__":
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_all_tasks())
-    loop.close()
+    run_all_tasks()
+    print_green("Successfully Ran playground!")
